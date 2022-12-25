ee3147e603d34edbb1d8c544ce35532755ffbc29
DSamniashvili <deasamniashvili@gmail.com>
2022-12-25 21:38:01 +0400

    created landing page

--- components/diff/AppEventBanner/AppEventBanner.spec.tsx
+++ components/diff/AppEventBanner/AppEventBanner.spec.tsx
@@ -3,26 +3,7 @@ import AppEventBanner from './AppEventBanner';
 
 describe('[Component] AppEventBanner', () => {
    it('should render component', () => {
-      render(<AppEventBanner />);
-
-      expect(screen.getByText('test-title')).toBeInTheDocument();
-   });
-
-   it('should render correct title', () => {
-      render(<AppEventBanner />);
-
-      expect(screen.getByText('test-title')).toBeInTheDocument();
-   });
-
-   it('should render correct description', () => {
-      render(<AppEventBanner />);
-
-      expect(screen.getByText('test-description')).toBeInTheDocument();
-   });
-
-   it('should render correct linkText', () => {
-      render(<AppEventBanner />);
-
-      expect(screen.getByText('test-linkText')).toBeInTheDocument();
+      const {container} = render(<AppEventBanner />);
+      expect(container).toMatchSnapshot();
    });
 });
--- components/diff/PlatformAndVisiblity/PlatFormAndVisiblity.spec.tsx
+++ components/diff/PlatformAndVisiblity/PlatFormAndVisiblity.spec.tsx
@@ -3,21 +3,7 @@ import PlatFormAndVisiblity from './PlatFormAndVisiblity';
 
 describe('[Component] PlatFormAndVisiblity', () => {
    it('should render platform transltions correctly', () => {
-      render(<PlatFormAndVisiblity />);
-
-      expect(screen.getByText('platform-title-mock')).toBeInTheDocument();
-      expect(screen.getByText('platform-description-mock')).toBeInTheDocument();
-      expect(screen.getByText('platform-button-text-mock')).toBeInTheDocument();
-   });
-   it('should render visiblity transltions correctly', () => {
-      render(<PlatFormAndVisiblity />);
-
-      expect(screen.getByText('visibility-title-mock')).toBeInTheDocument();
-      expect(
-         screen.getByText('visibility-description-mock'),
-      ).toBeInTheDocument();
-      expect(
-         screen.getByText('visibility-button-text-mock'),
-      ).toBeInTheDocument();
+      const {container} = render(<PlatFormAndVisiblity />);
+      expect(container).toMatchSnapshot();
    });
 });
